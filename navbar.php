2d8b1ab14546a83bbb51fe0d375423d667f5ce6e
Renata Nabillah <90308010+renatanbllh@users.noreply.github.com>
2021-11-13 20:03:41 +0700

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,31 @@
+ <!-- NAVBAR -->
+ <nav class="navbar navbar-light" style="background-color: #rgb(245, 100, 148);">
+  <a class="navbar-brand" href="#">Selamat Datang</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav ms-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="akademik.php">Akademik</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="akreditasi.php">Akreditasi</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+          Profile
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="sejarah.php">Sejarah</a>
+          <a class="dropdown-item" href="visimisi.php">Visi Misi</a>
+          <a class="dropdown-item" href="dosen.php">Dosen & Staff</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <!-- AKHIR NAVBAR -->
---
+++ sejarah.php
@@ -0,0 +1,202 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+
+    <!-- MY CSS -->
+    <link rel="stylesheet" href="style.css"/>
+
+    <style>
+      body{
+            background-color: rgb(245, 100, 148);
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+            background-attachment: fixed;
+            height: 100%;
+        }
+        .title{
+            text-align: center;
+            font-size: 2.5em;
+            color: #000;
+        }
+        .google-maps {
+          position: relative;
+          padding-bottom: 65%; 
+          height: 0;
+          overflow: hidden;
+      }
+      .google-maps iframe {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100% !important;
+          height: 100% !important;
+      }
+    </style>
+
+<link rel=”icon” href=”logoupi.jpg”>
+    <title>Sejarah</title>
+  </head>
+  <body>
+
+ <!-- NAVBAR -->
+ <nav class="navbar navbar-light" style="background-color: #rgb(245, 100, 148);">
+ <img src="laut.png" width="30" height="30" class="d-inline-block align-top" alt="">
+  <a class="navbar-brand" href="#">Selamat Datang</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav ms-auto">
+      <li class="nav-item ">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="akademik.php">Akademik</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="akreditasi.php">Akreditasi</a>
+      </li>
+      <li class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-expanded="false">
+          Profile
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="sejarah.php">Sejarah</a>
+          <a class="dropdown-item" href="visimisi.php">Visi Misi</a>
+          <a class="dropdown-item" href="dosen.php">Dosen & Staff</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <!-- AKHIR NAVBAR -->
+
+
+<!-- JUMBOTRON -->
+<nav class="jumbotron text-center">
+  <br>
+  <img src="logoupi.jpg" alt="universitas pendidikan indonesia" width="200" class="rounded-circle"> 
+  <h1 class="display-4">UNIVERSITAS PENDIDIKAN INDONESIA</h1>
+  <p class="lead">UPI KAMPUS SERANG, SISTEM INFORMASI KELAUTAN</p>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,128L48,122.7C96,117,192,107,288,117.3C384,128,480,160,576,181.3C672,203,768,213,864,202.7C960,192,1056,160,1152,154.7C1248,149,1344,171,1392,181.3L1440,192L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+</nav>
+<!-- AKHIR JUMBOTRON -->
+
+
+<!-- GAMBAR  -->
+<img src="header1.jpg" class="img-fluid" alt="...">
+<!-- AKHIR GAMBAR  -->
+
+<!-- ISI -->
+<div class="container">
+          <div class="row">
+              <div class="col text-center text-white">
+                  <h1>Sejarah</h1>
+              </div>
+          </div>
+
+          <div class="row">
+            <div class="col text-white">
+                <p> Universitas Pendidikan Indonesia (UPI) sebagai universitas pelopor dan unggul perlu berkontribusi dalam bidang tersebut dan juga dalam rangka UPI sebagai Center of Excellence bidang Pendidikan Vokasi. Kemudian, dalam rangka mempersiapkan sumberdaya manusia sektor kemaritiman pada era revolusi industri 4.0 UPI pada tahun 2019 berencana membuka program studi Sistem Informasi Kelautan.
+
+                    Sistem Informasi Kelautan merupakan bagian dari Ilmu Kelautan yang fokus pada analisis data kelautan baik numerik maupun spasial untuk kemudian diintegrasikan ke dalam suatu sistem informasi yang dikembangkan sebagai dasar pengambilan keputusan. Sumberdaya manusia yang terampil dalam hal tersebut sangat diperlukan dalam pembangunan di sektor kemaritiman pada industri 4.0.</p>
+      </div>
+<!-- AKHIR ISI -->
+
+<!-- RECOGNATION -->
+<span class="d-block p-2 bg-light.bg-gradient text-light"></span>
+  <div class="container px-4 py-5" id="featured-3">
+    <h2 class="pb-2 border-bottom text-white">Recognation</h2>
+    <div class="row g-3 py-4 row-cols-3 row-cols-lg-4 text-white">
+      <div class=" col">
+          <a class="navbar-brand" href="https://spot.upi.edu">
+          <img src="spot.png" class="img-fluid" alt="" width="160" height="120" >
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- AKHIR RECOGNATION -->
+
+<!-- MAPS -->
+  <span class="d-block p-2 bg-light.bg-gradient"></span>
+  <div class="container-xxl bg-light">
+    <div class="container-lg bg-light">
+      <footer class="py-5">
+        <div class="row">
+          <div class="col-4">
+            <h4 class=" text-black">TAUTAN</h4>
+            <table align="left">
+              <td>
+                  <tr>
+                    <ul class="nav flex-column">
+                      <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">UPI Bandung</a></li>
+                      <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Kampus UPI Serang</a></li>
+                      <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Kampus UPI Cibiru</a></li>
+                      <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Kampus UPI Tasikmalaya</a></li>
+                      <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Kampus UPI Purwakarta</a></li>
+                      <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Kampus UPI Sumedang</a></li>
+                      </ul>
+                  </tr>
+              </td>
+            </table>
+          </div> 
+          <div class="col-3">
+            <h4 class=" text-black">CONTACT US</h4>
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Universitas Pendidikan Indonesia Kampus Serang</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Jalan Ciracas 38, Serang </a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">(0254) 2002777</a></li>
+              <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">sik_kdserang@upi.edu</a></li>
+            </ul>
+          </div>
+    
+          <div class="col-4 offset-1">
+            <div class="col px-2 py-3">
+              <div class="google-maps">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3967.0370422558185!2d106.14447451430932!3d-6.125717661768984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e418adaa4f7f563%3A0x950ec58123df8596!2sUniversitas%20Pendidikan%20Indonesia%20(UPI)%20Kampus%20Serang!5e0!3m2!1sen!2sid!4v1635955511218!5m2!1sen!2sid" width="600" height="500" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+        <div class="d-flex justify-content-between py-1 my-1 border-top">
+          <p class="text-center">&copy; 2021 Sistem Informasi Kelautan UPI, Inc. All rights reserved.</p>
+          <ul class="list-unstyled d-flex">
+            <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#twitter"/></svg></a></li>
+            <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#instagram"/></svg></a></li>
+            <li class="ms-3"><a class="link-dark" href="#"><svg class="bi" width="24" height="24"><use xlink:href="#facebook"/></svg></a></li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous">
+    </script>
+<!-- AKHIR MAPS -->
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
